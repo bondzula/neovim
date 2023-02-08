33324a794a3f85dd6970ada7c3e33b6f8b512abd
Stefan Bondzulic <stefanbondzulic@gmail.com>
2023-02-08 22:45:58 +0100

    feat(LSP): adjust diagnostics
    - change the config to sort by severity
    - show icons instead of letters

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,6 +7,24 @@ local servers = {
   "tsserver", "rnix", "intelephense", "rust_analyzer", "sqls", "pyright", "tailwindcss", "terraformls", "volar",
 }
 
+-- General LSP settings
+-- Diagnostics config (disable virtual text, and sort by serverity)
+vim.diagnostic.config({
+  signs = true,
+  underline = true,
+  virtual_text = false,
+  severity_sort = true,
+  update_in_insert = false,
+})
+
+-- Add icons for each diagnostic type
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
 return {
   {
     "folke/neodev.nvim",
@@ -23,14 +41,6 @@ return {
     },
     config = function()
       local lspconfig = require("lspconfig")
-
-      vim.lsp.handlers["textDocument/publishDiagnostics"] =
-      vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-        virtual_text = false,
-        signs = true,
-        underline = true,
-        update_in_insert = false,
-      })
 
       vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
         border = "rounded",
