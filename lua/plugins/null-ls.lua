95b8ac5e6cb5365a5d3aed1f0b2a60cc9ef68a75
Stefan Bondzulic <stefanbondzulic@gmail.com>
2023-02-08 23:28:52 +0100

    feat: improvement's to null-ls and spell checking

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,6 +1,3 @@
-local on_attach = require("config.lsp-config").on_attach
-local capabilities = require("config.lsp-config").capabilities
-
 return {
   {
     "jose-elias-alvarez/null-ls.nvim",
@@ -8,43 +5,55 @@ return {
       { "neovim/nvim-lspconfig" },
     },
     config = function()
-      local null_ls = require('null-ls')
+      local null_ls = require("null-ls")
 
-      -- Get the dictionary
-      local spellingConfig = { extra_args = { "--config", vim.fn.expand("~/.config/nvim/cspell.json") } }
+      -- -- Get the dictionary
+      local spellingConfig = { "--config", vim.fn.expand("~/.config/nvim/cspell.json") }
 
-      local ltrsSettings = {
-        filetypes = { "latex", "tex", "bib", "markdown", "gitcommit", "text", "NeogitCommitMessage", "octo" },
+      -- Info diagnostic config
+      local diagnosticConfig = {
+        signs = true,
+        underline = true,
+        virtual_text = false,
+        severity_sort = true,
+        update_in_insert = false,
       }
 
+      -- Info severity postprocessing
+      local infoSeverity = function(diagnostic)
+        diagnostic.severity = vim.diagnostic.severity["INFO"]
+      end
+
+      -- file types for language tool
+      local ltrsFieTypes = { "latex", "tex", "bib", "markdown", "gitcommit", "text", "NeogitCommitMessage", "octo" }
+
+      -- Th is a spelling test for cspell
       null_ls.setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
+        on_attach = require("config.lsp-config").on_attach,
+        capabilities = require("config.lsp-config").capabilities,
         sources = {
           -- Check the spelling
-          null_ls.builtins.diagnostics.cspell.with({
-            extra_args = { "--config", spellingConfig },
-            diagnostic_config = {
-              underline = true,
-              virtual_text = false,
-              signs = true,
-              update_in_insert = false,
-              severity_sort = true,
-            },
-            diagnostics_postprocess = function(diagnostic)
-              diagnostic.severity = vim.diagnostic.severity["INFO"]
-            end,
-          }),
           null_ls.builtins.code_actions.cspell.with({
-            extra_args = { "--config", spellingConfig },
+            extra_args = spellingConfig,
+          }),
+          null_ls.builtins.diagnostics.cspell.with({
+            extra_args = spellingConfig,
+            diagnostic_config = diagnosticConfig,
+            diagnostics_postprocess = infoSeverity,
+          }),
+
+          -- Grammar checker
+          null_ls.builtins.code_actions.ltrs.with({
+            filetypes = ltrsFieTypes,
+          }),
+          null_ls.builtins.diagnostics.ltrs.with({
+            filetypes = ltrsFieTypes,
+            diagnostic_config = diagnosticConfig,
+            diagnostics_postprocess = infoSeverity,
           }),
 
           -- PHP
           null_ls.builtins.diagnostics.phpstan,
-
-          -- Grammar checker
-          null_ls.builtins.diagnostics.ltrs.with(ltrsSettings),
-          null_ls.builtins.code_actions.ltrs.with(ltrsSettings),
         },
       })
     end
