a75a825298848c09200f14d338c4ece647185b1a
Stefan Bondzulic <stefanbondzulic@gmail.com>
2023-02-09 23:33:21 +0100

    feat(plugin): add autolist.nvim package
    resolves #21

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -329,6 +329,36 @@ return {
     },
   },
 
+  {
+    "gaoDean/autolist.nvim",
+    ft = {
+      "markdown",
+      "text",
+      "tex",
+      "plaintex",
+      "octo",
+      "gitcommit",
+      "NeogitCommitMessage",
+    },
+    config = function()
+      local autolist = require("autolist")
+      autolist.setup()
+      autolist.create_mapping_hook("i", "<CR>", autolist.new)
+      autolist.create_mapping_hook("i", "<Tab>", autolist.indent)
+      autolist.create_mapping_hook("i", "<S-Tab>", autolist.indent, "<C-D>")
+      autolist.create_mapping_hook("n", "o", autolist.new)
+      autolist.create_mapping_hook("n", "O", autolist.new_before)
+      autolist.create_mapping_hook("n", ">>", autolist.indent)
+      autolist.create_mapping_hook("n", "<<", autolist.indent)
+      vim.api.nvim_create_autocmd("TextChanged", {
+        pattern = "*",
+        callback = function()
+          vim.cmd.normal({autolist.force_recalculate(nil, nil), bang = false})
+        end
+      })
+    end,
+  },
+
   -- TODO: Remove move this to another file
   -- Urlview
   {
