2a62aea585ff0eb3b9ca5fc2cb1155afb0b3df1c
Stefan Bondzulic <stefanbondzulic@gmail.com>
2023-02-10 18:18:54 +0100

    feat(leap): improve the leap, and add new plugins

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -312,11 +312,50 @@ return {
   -- Figure out where does this belong to?
   {
     "ggandor/leap.nvim",
-    keys = {
-      { "<leader>j", function() require("leap").leap({}) end, desc = "Leap" },
+    dependencies = {
+      { "tpope/vim-repeat" },
     },
+    keys = {
+      { "s" }, { "S" }
+    },
+    config = function()
+      require('leap').add_default_mappings()
+    end
   },
 
+  -- Improve line jumps
+  {
+    "ggandor/flit.nvim",
+    keys = {
+      { "f" }, { "F" }, { "t" }, { "T" }
+    },
+    config = function()
+      require('flit').setup {
+        keys = { f = 'f', F = 'F', t = 't', T = 'T' },
+        -- A string like "nv", "nvo", "o", etc.
+        labeled_modes = "v",
+        multiline = false,
+      }
+    end
+  },
+
+  -- Get all fency doing things without even moving
+  {
+    "ggandor/leap-spooky.nvim",
+    config = function()
+      require('leap-spooky').setup {
+        affixes = {
+          remote   = { window = 'r', cross_window = 'R' },
+          magnetic = { window = 'm', cross_window = 'M' },
+        },
+        -- If this option is set to true, the yanked text will automatically be pasted
+        -- at the cursor position if the unnamed register is in use.
+        paste_on_remote_yank = false,
+      }
+    end
+  },
+
+  -- Regex explainer
   {
     "bennypowers/nvim-regexplainer",
     dependencies = {
