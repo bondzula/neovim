707304ca38fb3b1633981dd304b399d913b8f3d3
Stefan Bondzulic <stefanbondzulic@gmail.com>
2023-02-23 13:43:40 +0100

    fix(swap): disable swap file
    resolves #30 and #32

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -49,6 +49,11 @@ opt.wildmode = "longest:full,full" -- Command-line completion mode
 opt.winminwidth = 5 -- Minimum window width
 opt.wrap = false -- Disable line wrap
 
+-- Disable swap, and backup files
+opt.swapfile = false
+opt.backup = false
+opt.writebackup = false
+
 if vim.fn.has("nvim-0.9.0") == 1 then
   opt.splitkeep = "screen"
   opt.shortmess:append({ C = true })
