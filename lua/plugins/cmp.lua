2a07dddecfe83a98402aa1a1eaba51dbbab6bdd0
Stefan Bondzulic <stefanbondzulic@gmail.com>
2023-03-09 23:10:08 +0100

    feat(cmp): key binds, luasnip, lazy load
    Move away from tab being used for everything, and move LuaSnip into its
    own config

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -11,54 +11,30 @@ return {
       { "hrsh7th/cmp-nvim-lsp-signature-help" },
       { "hrsh7th/cmp-path" },
 
-      { "L3MON4D3/LuaSnip" },
       { "saadparwaiz1/cmp_luasnip" },
-      { "rafamadriz/friendly-snippets" },
       { "onsails/lspkind-nvim" },
       { "petertriho/cmp-git" },
     },
-    config = function()
+    opts = function()
       local cmp = require("cmp")
-      local luasnip = require("luasnip")
 
-      cmp.setup({
+      return {
         snippet = {
           expand = function(args)
-            luasnip.lsp_expand(args.body)
+            require("luasnip").lsp_expand(args.body)
           end,
         },
         mapping = {
-          ["<C-p>"] = cmp.mapping.select_prev_item(),
-          ["<C-n>"] = cmp.mapping.select_next_item(),
-          ["<C-b>"] = cmp.mapping.scroll_docs( -4),
+          -- Navigate through completion menu
+          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+          -- Scroll docs for the currently selected item
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-e>"] = cmp.mapping.close(),
-          ["<CR>"] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Insert,
-            select = true
-          }),
-
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if require("copilot.suggestion").is_visible() then
-              require("copilot.suggestion").accept()
-            elseif cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable( -1) then
-              luasnip.jump( -1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
+          -- Close completion menu
+          ["<C-e>"] = cmp.mapping.abort(),
+          -- Confirm Completion
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
         },
         formatting = {
           format = require("lspkind").cmp_format(),
@@ -66,9 +42,9 @@ return {
         sources = {
           { name = "nvim_lsp" },
           { name = "nvim_lsp_signature_help" },
+          { name = "luasnip" },
           { name = "git" },
           { name = "path" },
-          { name = "luasnip" },
           { name = "nvim_lua" },
         },
         view = {
@@ -78,7 +54,12 @@ return {
           completion = cmp.config.window.bordered(),
           documentation = cmp.config.window.bordered()
         },
-      })
+      }
+    end,
+    config = function(_, opts)
+      local cmp = require("cmp")
+
+      cmp.setup(opts)
 
       cmp.setup.cmdline({ "/", "?" }, {
         mapping = cmp.mapping.preset.cmdline(), -- Tab for selection (arrows needed for selecting past items)
@@ -98,11 +79,6 @@ return {
           entries = "custom",
         },
       })
-
-      -- for friendly snippets
-      require("luasnip.loaders.from_vscode").lazy_load()
-      -- for custom snippets
-      require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/snips" } })
     end,
   },
 
