02244199422516f52ba0e7781b935d12c8145fab
Stefan Bondzulic <stefanbondzulic@gmail.com>
2023-03-09 23:03:26 +0100

    feat(copilot): set custom key binds
    This will improve separation of key binds between CMP, LuaSnip, and
    Copilot.

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,18 +3,23 @@ return {
   {
     "zbirenbaum/copilot.lua",
     event = "InsertEnter",
+    keys = {
+      { "<M-]>", function() require("copilot.suggestion").next() end,        mode = "i", desc = "Copilot Next" },
+      { "<M-[>", function() require("copilot.suggestion").prev() end,        mode = "i", desc = "Copilot Previous" },
+      { "<M-.>", function() require("copilot.suggestion").accept() end,      mode = "i", desc = "Copilot Accept" },
+      { "<M->>", function() require("copilot.suggestion").accept_line() end, mode = "i", desc = "Copilot Accept Line" },
+    },
     opts = {
       suggestion = {
         enabled = true,
-        auto_trigger = true,
+        auto_trigger = false,
         debounce = 75,
         keymap = {
           accept = false,
           accept_word = false,
           accept_line = false,
-          next = "<M-]>",
-          prev = "<M-[>",
-          dismiss = "<C-]>",
+          dismiss = false,
+          next = false,
         },
       },
     },
