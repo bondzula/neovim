47a7cb1f82c6210f113488edb0c5823421a68b73
Stefan Bondzulic <stefanbondzulic@gmail.com>
2023-03-09 16:05:31 +0100

    feat(splitjoin): switch to lua plugin that uses treesitter

--- lua/plugins/splitjoin.lua
+++
@@ -1,9 +0,0 @@
-return {
-  -- Batter split / join functionality
-  -- TODO: use new treesitter based join functionality
-  {
-    "AndrewRadev/splitjoin.vim",
-    keys = { { "gJ" }, { "gS" } },
-    event = "BufReadPost",
-  },
-}
