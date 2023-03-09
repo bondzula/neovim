15d43fe0fd3c608f898f15951cda66fc4ae54e37
Stefan Bondzulic <stefanbondzulic@gmail.com>
2023-03-09 23:11:25 +0100

    feat(lazy): improve config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -15,7 +15,34 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- Load the plugins from plugins directory
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", {
+  change_detection = {
+    enabled = true,
+    notify = false,
+  },
+  performance = {
+    cache = {
+      enabled = true,
+    },
+    reset_packpath = true, -- reset the package path to improve startup time
+    rtp = {
+      reset = true,        -- reset the runtime path to $VIMRUNTIME and your config directory
+      ---@type string[]
+      paths = {},          -- add any custom paths here that you want to includes in the rtp
+      ---@type string[] list any plugins you want to disable here
+      disabled_plugins = {
+        -- "gzip",
+        -- "matchit",
+        -- "matchparen",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        -- "zipPlugin",
+      },
+    },
+  },
+})
 
 -- Setup the colorscheme after the plugin setup
 vim.cmd.colorscheme("catppuccin")
