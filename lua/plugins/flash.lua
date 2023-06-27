7d9e7f9eb5952083f495152d169d9396255f199e
Stefan Bondzulic <stefanbondzulic@gmail.com>
2023-06-27 09:08:10 +0200

    feat: add treesitter search to flash.nvim

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -29,6 +29,14 @@ return {
         end,
         desc = "Remote Flash",
       },
+      {
+        "R",
+        mode = { "n", "o", "x" },
+        function()
+          require("flash").treesitter_search()
+        end,
+        desc = "Treesitter Search",
+      },
     },
   },
 }
