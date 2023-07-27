04bb5f7443a77dd77cef8d7cbe8c525aa5f4655f
Stefan Bondzulic <stefanbondzulic@gmail.com>
2023-07-27 12:00:01 +0200

    feat(null-ls): disable null-ls

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -3,4 +3,5 @@ return {
   { "echasnovski/mini.pairs", enabled = false },
   { "echasnovski/mini.surround", enabled = false },
   { "nvim-pack/nvim-spectre", enabled = false },
+  { "jose-elias-alvarez/null-ls.nvim", enabled = false },
 }
