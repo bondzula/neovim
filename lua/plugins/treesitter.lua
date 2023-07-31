4d6b4f39f49a27d9a342827d5ff79b4d61bb2278
Stefan Bondzulic <stefanbondzulic@gmail.com>
2023-07-31 23:31:35 +0200

    feat(treesitter): add more parsers

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,27 @@
+return {
+  -- add more treesitter parsers
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "astro",
+        "css",
+        "dockerfile",
+        "gitcommit",
+        "gitignore",
+        "git_rebase",
+        "go",
+        "hcl",
+        "kdl",
+        "nix",
+        "php",
+        "phpdoc",
+        "rust",
+        "sql",
+        "svelte",
+        "toml",
+        "vue",
+      },
+    },
+  },
+}
