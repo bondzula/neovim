ce074b654656c18a02f932657903ab968b076500
Stefan Bondzulic <stefanbondzulic@gmail.com>
2023-07-31 23:46:50 +0200

    feat(neogit): set <leader>gg as keybind

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -3,6 +3,9 @@ return {
     "NeogitOrg/neogit",
     dependencies = "nvim-lua/plenary.nvim",
     cmd = "Neogit",
+    keys = {
+      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit" },
+    },
     config = true,
   },
 }
